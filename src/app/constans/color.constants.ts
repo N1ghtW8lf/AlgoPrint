5f35aab396acd4c2eae424c8826d7cbb6c48eeba
N1ghtW8lf <Timastarik@outlook.com>
2025-02-23 17:47:25 +0500

    Initial Commit

---
+++ src/app/constans/color.constants.ts
@@ -0,0 +1,67 @@
+const COLORS = {
+    main: {
+      darkest: "#000000", // Самый Darker
+      need: {
+        darkestPurple: "#380355", // Самый тёмный purple
+        purple: [
+          "#5C0D88",
+          "#7726A3",
+          "#8F41BE",
+          "#A55ED9",
+          "#BB7CF4",
+          "#D19AFF",
+          "#E6B9FF",
+          "#F0D3FF", // Add a lighter shade if needed from your screenshot
+        ],
+        pinkishBlue: "#E4E7FF", // розовый (но не он & голубой)
+         lighter: "#FFFFFF"
+      },
+      purplePrimary: [
+        "#5C0D88",
+        "#7726A3",
+        "#8F41BE",
+        "#A55ED9",
+        "#BB7CF4",
+        "#D19AFF",
+        "#E6B9FF",
+        "#F0D3FF",
+        "#F6EBFF"
+      ],
+      darkGray: [
+        "#1C1C1C",
+        "#383838",
+        "#545454",
+        "#707070",
+        "#8C8C8C",
+        "#A8A8A8",
+        "#C4C4C4",
+        "#E0E0E0",
+        "#F8F8F8",
+      ],
+    },
+    background: {
+      sky: {
+        darker: "#D9D9D9",
+        normal: "#E6E6E6",
+        light: "#F2F2F2",
+        lighter: "#FAFAFA",
+      },
+    },
+    secondary: {
+      red: {
+        darker: "#0D1CD9",
+        normal: "#4554FF",
+        lighter: "#C2C8FF",
+        lightest: "#E1E4FF",
+      },
+      green: {
+        darker: "#D9D90D",
+        normal: "#FFFF45",
+        lighter: "#FFFFC2",
+        lightest: "#FFFFE1",
+      },
+    },
+  };
+  
+  
+  export default COLORS;
